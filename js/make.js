d571aa6febdc11df21bf8d52bf59aee885fa7527
jw4rd <j@w4rd.com>
2016-12-13 15:58:38 -0600

    snap to grid & make.js

---
+++ js/make.js
@@ -0,0 +1,52 @@
+var xmin = 0
+var ymin = 0
+var xmax = 0
+var ymax = 0
+
+function make(){
+
+	if(lines.length>0){
+		xmin=lines[0][0]
+		ymin=lines[0][1]
+		xmax=xmin
+		ymax=ymin
+		
+		for(i=0;i<lines.length;i++){
+			//min
+			if(lines[i][0]<xmin){
+				xmin = lines[i][0]
+			}
+			if(lines[i][2]<xmin){
+				xmin = lines[i][2]
+			}
+
+			if(lines[i][1]<ymin){
+				ymin = lines[i][1]
+			}
+			if(lines[i][3]<ymin){
+				ymin = lines[i][3]
+			}
+			//max
+			if(lines[i][0]>xmax){
+				xmax = lines[i][0]
+			}
+			if(lines[i][2]>xmax){
+				xmax = lines[i][2]
+			}
+
+			if(lines[i][1]>ymax){
+				ymax = lines[i][1]
+			}
+			if(lines[i][3]>ymax){
+				ymax = lines[i][3]
+			}
+
+		}
+	}
+	console.log(xmin,ymin)
+	console.log(xmax,ymax)
+	console.log(unit)
+	console.log(lines)
+
+
+}
